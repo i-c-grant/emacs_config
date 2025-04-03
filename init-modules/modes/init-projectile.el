87cdb8c2c200393628d9387a4b23bee124e5db54
Ian Grant <ian.conway.grant@gmail.com>
2025-04-03 19:40:09 -0400

    keybinding: set projectile-command-map "s r" to call consult-ripgrep

--- init-modules/modes/init-projectile.el
+++ init-modules/modes/init-projectile.el
@@ -2,4 +2,7 @@
   :config
   (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map))
 
+;; Consult does some things better than projectile
+(define-key projectile-command-map (kbd "s r") 'consult-ripgrep)
+
 (provide 'init-projectile)
