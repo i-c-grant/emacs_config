166a63f9a98ba5dc9526670c096a898b8ce642ae
Ian Grant <ian.conway.grant@gmail.com>
2025-04-15 00:09:16 -0400

    added frecency-supporting function from minad

--- init-modules/modes/init-vertico.el
+++ init-modules/modes/init-vertico.el
@@ -13,4 +13,18 @@
   (vertico-next)
   (vertico-exit))
 
+;; From minad in https://www.reddit.com/r/emacs/comments/1js6xvw/complement_corfu_vertico_and_completionpreview/
+;; Best of both worlds: Only delete old duplicates, such that rare candidates
+;; are not lost. At the same time ensure that ranking by frecency works.
+(defun +history-delete-old-duplicates (var elt &rest _)
+  (when-let ((hist (symbol-value var))
+             ((and (listp hist) (integerp history-length)))
+             (old (nthcdr (/ history-length 2) hist)))
+    (setcdr old (delete elt (cdr old)))))
+(advice-add #'add-to-history :before #'+history-delete-old-duplicates)
+(setq history-delete-duplicates nil)
+
+;; Also use savehist
+(use-package savehist)
+
 (provide 'init-vertico)
